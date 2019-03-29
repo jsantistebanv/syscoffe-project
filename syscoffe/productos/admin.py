c2453e681417cbb2544c8c079edd86c9c3a4530c
Jose Ramirez <jcramireztello@gmail.com>
2019-03-28 19:52:17 -0500

    agregando campos de tipo imagen y booleanos

--- syscoffe/productos/admin.py
+++ syscoffe/productos/admin.py
@@ -9,8 +9,17 @@ class CategoriaAdmin(admin.ModelAdmin):
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'categoria',)
+    list_display = ('get_imagen', 'nombre', 'categoria', 'es_activo')
     list_filter = ('categoria',)
+    list_display_links = ('nombre',)
     search_fields = ('nombre',)
+    readonly_fields = ('fecha_creado', 'fecha_modificado')
+
+    def get_imagen(self, obj):
+        url = obj.imagen.url if obj.imagen else ''
+        return '<img src="{}" width="60px" height="auto">'.format(url)
+
+    get_imagen.allow_tags = True
+    get_imagen.short_description = 'Imagen'
 
 
--- syscoffe/productos/models.py
+++ syscoffe/productos/models.py
@@ -13,9 +13,12 @@ class Categoria(models.Model):
 
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
+    es_activo = models.BooleanField(default=True)
     nombre = models.CharField(max_length=50)
-    categoria = models.ForeignKey(Categoria, related_name='productos')
+    categoria = models.ForeignKey(Categoria, related_name='productos') # producto_set
+    precio = models.FloatField()
     descripcion = models.TextField(max_length=500, blank=True, null=True, default=None)
+    imagen = models.ImageField(upload_to='producto/', blank=True, null=True, default=None)
     fecha_creado = models.DateTimeField(auto_now_add=True)
     fecha_modificado = models.DateTimeField(auto_now=True)
 
