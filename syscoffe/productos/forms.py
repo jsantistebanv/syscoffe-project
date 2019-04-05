f1928978ea8c155dfde1c1bd9c40805917848edd
Jose Ramirez <jcramireztello@gmail.com>
2019-04-04 19:55:27 -0500

    - aplicando validaciones por campo - personalizar widgets y atributos en el formulario

--- syscoffe/productos/forms.py
+++ syscoffe/productos/forms.py
@@ -9,6 +9,19 @@ class CategoriaForm(forms.ModelForm):
 
 
 class ProductoForm(forms.ModelForm):
+    descripcion = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}), required=False, max_length=50)
+    
     class Meta:
         model = Producto
         fields = ('nombre', 'categoria', 'precio', 'descripcion', 'imagen')
+    
+    def __init__(self, *args, **kwargs):
+        super(ProductoForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": "form-control"})
+
+    def clean_precio(self):
+        precio = self.cleaned_data.get('precio')
+        if precio < 0:
+            raise forms.ValidationError("El precio no puede ser menor a 0")
+        return precio
